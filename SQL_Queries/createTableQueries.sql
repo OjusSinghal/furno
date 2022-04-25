331b6c6b0c13995b4f85c95444959fd49691585a
Ojus Singhal <ojus.singhal17@gmail.com>
2022-04-25 17:05:47 +0530

    updated create table queries

--- SQL_Queries/createTableQueries.sql
+++ SQL_Queries/createTableQueries.sql
@@ -1,159 +1,183 @@
-use furnodb;
+CREATE TABLE buyer (
+    buyerID CHAR(10),
+    firstName VARCHAR(30) NOT NULL,
+    middleName VARCHAR(30),
+    lastName VARCHAR(30),
+    buyerEmailID VARCHAR(50) NOT NULL UNIQUE,
+    buyerPassword VARCHAR(22) NOT NULL,
+    buyerContactNumber NUMERIC(10 , 0 ) NOT NULL,
+    dob DATE NOT NULL,
+    gender ENUM('M', 'F', 'N', 'U'),
+    -- Male, Female, Non_Binary, Unspecified
+    cardNumbers JSON,
+    PRIMARY KEY (buyerID),
+    CHECK (buyerID LIKE 'B%'),
+    CHECK (2022 - YEAR(dob) >= 15),
+    CHECK (buyerPassword LIKE '%[A-Z]%'
+        AND buyerPassword LIKE '%[a-z]%'
+        AND buyerPassword LIKE '%[!@#$%a^&*()-_+=.,;:"`~]%'
+        AND (LENGTH(buyerPassword) >= 8))
+);
 
-create table buyer (
-    buyerID char(10) primary key,
-    firstName varchar(50) not null,
-    secondName varchar(50),
-    lastName varchar(50),
-    dob date not null,
-    gender enum("M", "F", "T", "O", "U"),
-    # "Male", "Female", "Transgender", "Other", "Unspecified" 
-    contactNumber numeric(10,0) not null,
-    email varchar(200) not null UNIQUE,
-    buyerPassword varchar(50) not null,
-    cardNumbers json,
-    check (buyerID like "B%"),
-    check (2022 - year(dob) >= 15)
+
+
+CREATE TABLE seller (
+    sellerID CHAR(10),
+    sellerName VARCHAR(30) NOT NULL,
+    sellerEmailID VARCHAR(50) NOT NULL UNIQUE,
+    sellerPassword VARCHAR(22) NOT NULL,
+    sellerContactNumber NUMERIC(10 , 0 ) NOT NULL,
+    GST NUMERIC(15 , 0 ) UNIQUE,
+    PRIMARY KEY (sellerID),
+    CHECK (sellerID LIKE 'S%'),
+    CHECK (sellerPassword LIKE '%[A-Z]%'
+        AND sellerPassword LIKE '%[a-z]%'
+        AND sellerPassword LIKE '%[!@#$%a^&*()-_+=.,;:"`~]%'
+        AND LENGTH(sellerPassword) >= 8)
 );
  
- 
-create table seller (
-    sellerID char(10) PRIMARY KEY,
-    userPassword VARCHAR(50) NOT NULL,
-    userContactNumber NUMERIC(10,0) NOT NULL,
-    userEmailID VARCHAR(200) NOT NULL UNIQUE,
-    sellerName VARCHAR(200) NOT NULL,
-    GST NUMERIC(15,0) UNIQUE,
-    check (sellerID like "S%")
+CREATE TABLE userAddress (
+    addressID CHAR(10),
+    street VARCHAR(50) NOT NULL,
+    city VARCHAR(50) NOT NULL,
+    userState VARCHAR(50) NOT NULL,
+    zip NUMERIC(6 , 0 ) NOT NULL,
+    PRIMARY KEY (addressID)
 );
- 
-create TABLE userAddress (
-    addressID CHAR(10) PRIMARY KEY,
-    street VARCHAR(100) NOT NULL,
-    city VARCHAR(100) NOT NULL,
-    userState VARCHAR(100) NOT NULL,
-    zip CHAR(6) NOT NULL
-);
- 
-create TABLE sellerResidesIn (
+
+CREATE TABLE sellerResidesIn (
     addressID CHAR(10),
     sellerID CHAR(10),
-    PRIMARY KEY (sellerID, addressID),
-    FOREIGN KEY (sellerID) REFERENCES seller(sellerID),
-    FOREIGN KEY (addressID) REFERENCES userAddress(addressID)
+    PRIMARY KEY (sellerID , addressID),
+    FOREIGN KEY (sellerID) REFERENCES seller (sellerID),
+    FOREIGN KEY (addressID) REFERENCES userAddress (addressID)
 );
  
-create TABLE buyerResidesIn (
+CREATE TABLE buyerResidesIn (
     addressID CHAR(10),
     buyerID CHAR(10),
-    PRIMARY KEY (buyerID, addressID),
-    FOREIGN KEY (buyerID) REFERENCES buyer(buyerID),
-    FOREIGN KEY (addressID) REFERENCES userAddress(addressID)
+    PRIMARY KEY (buyerID , addressID),
+    FOREIGN KEY (buyerID) REFERENCES buyer (buyerID),
+    FOREIGN KEY (addressID) REFERENCES userAddress (addressID)
 );
 
  
-create table promoCode (
-   codeName varchar(15) primary key,
-   minimumPrice int not null,
-   discountMultiplier numeric(5, 4) not null,
-   expiryDate timestamp,
-   activeStatus boolean not null,
-   remainingRedeems int,
-   check (minimumPrice >= 0),
-   check (remainingRedeems >= 0)
+CREATE TABLE promoCode (
+    codeName VARCHAR(15),
+    minimumPrice INT NOT NULL,
+    discountMultiplier NUMERIC(5 , 4 ) NOT NULL,
+    expiryDate TIMESTAMP,
+    activeStatus BOOLEAN NOT NULL,
+    remainingRedeems INT,
+    PRIMARY KEY (codeName),
+    CHECK (minimumPrice >= 0),
+    CHECK (remainingRedeems >= 0),
+    CHECK (discountMultiplier >= 0.0
+        AND discountMultiplier <= 1.0)
 );
 
  
-create table product(
-  productID char(16) primary key,
-  productName varchar(20) not null,
-  aboutProduct varchar(100) not null,
-  category enum ("Beds", "Mattresses", "Chairs", "Tables", "Dining", "Sofas", "Wardrobes"),
-  dimensionsHeight numeric(4, 2),
-  dimensionsWidth numeric(4, 2),
-  dimensionsLength numeric(4, 2),
-  productWeight numeric(4, 2),
-  material varchar(20),
-  brand varchar(20),
-  avgRating numeric(3, 2),
-  discountMultiplier numeric(4, 3) not null,
-  stock int not null,
-  sellerID char(10) not null,
-  images blob,
-  foreign key (sellerID) references seller(sellerID),
-  check (dimensionsHeight > 0.0),
-  check (dimensionsWidth > 0.0),
-  check (dimensionsLength > 0.0),
-  check (productWeight > 0.0),
-  check (avgRating >= 0.0 and avgRating <= 5.0)  
+CREATE TABLE product (
+    productID CHAR(16),
+    sellerID CHAR(10) NOT NULL,
+    productName VARCHAR(20) NOT NULL,
+    aboutProduct VARCHAR(200) NOT NULL,
+    category ENUM('Beds', 'Mattresses', 'Chairs', 'Tables', 'Dining', 'Sofas', 'Wardrobes', 'Other') NOT NULL,
+    dimensionsHeight NUMERIC(4 , 2 ),
+    dimensionsWidth NUMERIC(4 , 2 ),
+    dimensionsLength NUMERIC(4 , 2 ),
+    productWeight NUMERIC(6 , 2 ),
+    material VARCHAR(20),
+    brand VARCHAR(20),
+    avgRating NUMERIC(3 , 2 ),
+    discountMultiplier NUMERIC(4 , 3 ) NOT NULL,
+    stock INT NOT NULL,
+    images BLOB,
+    PRIMARY KEY (productID),
+    FOREIGN KEY (sellerID) REFERENCES seller (sellerID),
+    CHECK (dimensionsHeight > 0.0
+        AND dimensionsWidth > 0.0
+        AND dimensionsLength > 0.0),
+    CHECK (productWeight > 0.0),
+    CHECK (avgRating >= 0.0 AND avgRating <= 5.0),
+    CHECK (stock >= 0),
+    CHECK (discountMultiplier >= 0.0
+        AND discountMultiplier <= 1.0)
 );
  
  
  
-create TABLE selects (
+CREATE TABLE putsIntoCart (
     buyerID CHAR(10),
     productID CHAR(10),
     quantity INT NOT NULL,
-    FOREIGN KEY (buyerID) REFERENCES buyer(buyerID),
-    FOREIGN KEY (productID) REFERENCES product(productID),
-    PRIMARY KEY(buyerID, productID)
+    FOREIGN KEY (buyerID) REFERENCES buyer (buyerID),
+    FOREIGN KEY (productID) REFERENCES product (productID),
+    PRIMARY KEY (buyerID , productID),
+    CHECK (quantity > 0)
 );
+
+CREATE TABLE orders (
+    orderID CHAR(16),
+    totalPayment INT NOT NULL,
+    orderTime TIMESTAMP NOT NULL,
+    fullfillmentStatus ENUM('U', 'D', 'R', 'C') NOT NULL,
+    amountDue INT NOT NULL,
+    deliveryTime TIMESTAMP,
+    trackingID CHAR(16) UNIQUE,
+    addressID CHAR(10) NOT NULL,
+    paymentMethod VARCHAR(16) NOT NULL,
+    buyerID CHAR(16) NOT NULL,
+    codeName VARCHAR(15),
+    PRIMARY KEY (orderID),
+    FOREIGN KEY (codeName)
+        REFERENCES promoCode (codeName),
+    FOREIGN KEY (addressID)
+        REFERENCES userAddress (addressID),
+    FOREIGN KEY (buyerID)
+        REFERENCES buyer (buyerID),
+    CHECK (deliveryTime > orderTime),
+    CHECK (totalPayment > 0)
+);
+
+CREATE TABLE containsProduct (
+    orderID CHAR(16),
+    productID CHAR(16),
+    quantity INT NOT NULL,
+    fulfillmentStatus ENUM('U', 'D', 'R', 'C') NOT NULL,
+    PRIMARY KEY (orderID , productID),
+    FOREIGN KEY (orderID)
+        REFERENCES orders (orderID),
+    FOREIGN KEY (productID)
+        REFERENCES product (productID),
+    CHECK (quantity > 0)
+);
+-- #"Unfulfilled", "Delivered", "Returned", "Canceled"
  
-create TABLE lists (
-    sellerID CHAR(10),
-    productID CHAR(10),
-    stock INT NOT NULL,
-    discountMultiplier INT,
-    FOREIGN KEY (sellerID) REFERENCES seller(sellerID),
-    FOREIGN KEY (productID) REFERENCES product(productID),
-    PRIMARY KEY(sellerID, productID),
-    check (stock >= 1)
-);
-
-create table orders(
-   orderID char(16) primary key,
-   totalPayment int not null,
-   orderTime timestamp not null,
-   fullfillmentStatus enum("U", "D", "R", "C") not null,
-   #"Unfulfilled", "Delivered", "Returned", "Canceled"
-   amountDue int not null,
-   deliveryTime timestamp,
-   trackingID char(16) unique,
-   # trackingID is different from orderID because it might change for the same order
-   addressID char(10) not null,
-   paymentMethod varchar(16) not null,
-   # this is 0 when cash, 16 digit card number when card
-   buyerID char(16) not null,
-   codeName varchar(15),
-   check (deliveryTime > orderTime),
-   foreign key (codeName) references promoCode(codeName),
-   foreign key (addressID) references userAddress(addressID),
-   foreign key (buyerID) references buyer(buyerID)
-);
-
-create table containsProduct (
-   orderID char(16),
-   productID char(16),
-   quantity int not null,
-   fulfillmentStatus enum("U", "D", "R", "C") not null,
-   -- #"Unfulfilled", "Delivered", "Returned", "Canceled"
-   primary key (orderID, productID),
-   foreign key (orderID) references orders(orderID),
-   foreign key (productID) references product(productID),
-   check (quantity > 0)
-);
-
- 
-create table reviews(
-   reviewID char(16) primary key,
-   reviewContent varchar(500),
-   rating numeric(1, 0) not null,
-   reviewTime timestamp not null,
-   productID char(16),
-   foreign key (productID) references product(productID),
-   check (rating >= 0 and rating <= 5)
+CREATE TABLE reviews (
+    buyerID CHAR(10),
+    productID CHAR(16),
+    reviewContent VARCHAR(500),
+    rating NUMERIC(1 , 0 ) NOT NULL,
+    reviewTime TIMESTAMP NOT NULL,
+    PRIMARY KEY (buyerID , productID),
+    FOREIGN KEY (productID)
+        REFERENCES product (productID),
+    FOREIGN KEY (buyerID)
+        REFERENCES buyer (buyerID),
+    CHECK (rating >= 0 AND rating <= 5)
 );
  
  
 show tables;
-select * from seller;
+# drop table buyerResidesIn;
+# drop table sellerResidesIn;
+# drop table containsProduct;
+# drop table selects;
+# drop table reviews;
+# drop table orders;
+# drop table promoCode;
+# drop table buyer;
+# drop table userAddress;
+# drop table product;
+# drop table seller;
